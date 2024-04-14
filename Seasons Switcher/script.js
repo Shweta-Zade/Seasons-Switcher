cda0085da4951187ecaf679f2c6ec47eb2ea4f26
Shweta Vilas Zade <127298967+Shweta-Zade@users.noreply.github.com>
2024-04-14 21:23:17 +0530

    Add files via upload

---
+++ Seasons Switcher/script.js
@@ -0,0 +1,16 @@
+let startingImgEl = document.getElementById("startingImg");
+
+
+function Spring(){
+    startingImgEl.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+}
+
+function Summer(){
+    startingImgEl.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+}
+function Autumn(){
+    startingImgEl.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+}
+function Winter(){
+    startingImgEl.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+}
